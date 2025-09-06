0175c789bccb74a6714b13d84b42b8e6a5e3ca70
qwlp <sokpiseth.thin@gmail.com>
2025-09-06 20:39:55 +0700

    updated main

--- main.py
+++ main.py
@@ -3,7 +3,11 @@ def get_book_text(path_to_file):
             file_contents = f.read()
             return file_contents
 
+def get_word_count(book):
+    words = book.split()
+    return len(words)
+
 def main():
-    print(get_book_text("books/frankenstein.txt"))
+    print(f"{get_word_count(get_book_text("books/frankenstein.txt"))} words found in the document")
 
 main()
